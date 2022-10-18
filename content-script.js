d80b80aff724874a3306b63f29f8538dfc1dfd34
Anushka7310 <anushkaguptably@gmail.com>
2022-10-18 23:32:16 +0530

    update code logic to add random timing

--- content-script.js
+++ content-script.js
@@ -1,35 +1,44 @@
-async function sendConnectionRequest() {
-  // alert("test")
+function sendConnectionRequest() {
+  // selecting all buttons from the current page with id starting with "ember"
   let allButtons = document.querySelectorAll('*[id^="ember"]');
 
+  //empty array to store current buttons
   let connectButtons = [];
 
+  //filtering buttons namely "Connect from all buttons"
   allButtons.forEach((button) => {
     if (button.innerText === "Connect") {
+      //storing this value in the connectButtons array
       connectButtons.push(button);
     }
   });
-  connectButtons.forEach(async (button, index) => {
+
+  let time = 0;
+  connectButtons.forEach((button) => {
+    time = time + 5000;
+
+    //calculating random values from 1-5
+    let random = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+
+    //calcultes the time for next run
+    time = time + random * 1000;
+
+    //clicking of next button after predefined time gap
     setTimeout(() => {
       button.click();
-      console.log("Test");
+      //clicking of send button after sending connect request if exists
       setTimeout(() => {
         let newAllButtons = document.querySelectorAll('*[id^="ember"]');
         let sendButton = [];
         newAllButtons.forEach((button) => {
           if (button.innerText === "Send") {
-            console.log("send buttons");
             sendButton.push(button);
           }
         });
         sendButton.forEach((btn) => btn.click());
-      }, 500+index*100);
-    }, 1500+(index*1500));
+      }, 500);
+    }, time);
   });
-}
-
-function sleep2(ms) {
-  new Promise((res) => setTimeout(res, ms));
 }
 
 sendConnectionRequest();
